b77adf151074c941d4f1d61d8cdf24cda08ae87d
subintoto <subintoto@naver.com>
2022-06-13 14:28:45 +0900

    등록CL

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -1,9 +1,12 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.PhoneDao;
 import com.javaex.vo.PersonVo;
@@ -19,40 +22,55 @@ public class PhoneController {
 	// 메소드 gs
 
 	// 메소드 일반
+	//전화번호 리스트
+	@RequestMapping(value = "/list", method={RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		System.out.println("PhoneController>list()");
+		
+		
+		//Dao를 통해 PhoneList()를 가져온다
+		PhoneDao phoneDao = new PhoneDao();
+		List<PersonVo> personList = phoneDao.getPersonList();
+		
+		//ds데이터 보내기 --> request attribute에 넣는다.
+		model.addAttribute("personList", personList);
+	
+		return "/WEB-INF/views/list.jsp";
+	
+	}
+	
+	
+	
 	//전화번호 등록
-	@RequestMapping(value="/write", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value="/write", method={RequestMethod.GET, RequestMethod.POST})
 
-	public String write(@RequestParam("name") String name,
-						@RequestParam("hp") String hp,
-						@RequestParam("company") String company) {
+	public String write(@ModelAttribute PersonVo personVo) {
+	
 		System.out.println("PhoneController>write");
 		
-		
-		
-		//파라미터 꺼내기
-		System.out.println(name);
-		System.out.println(hp);
-		System.out.println(company);
-		
-		
-		
-		//vo로 묶기
-		PersonVo personVo = new PersonVo(name, hp, company);
+		//파라미터 꺼내기+vo로 묶기를 DS해서 메소드의 파라미타로 보내줌
 		System.out.println(personVo);
-		
-		
+	
 		
 		//dao로 저장하기
 		PhoneDao phoneDao = new PhoneDao();
 		int count = phoneDao.personInsert(personVo);
 		System.out.println(count);
-		return "";
+		
+		
+		//리다이렉트
+		//리스트로 리다이렉트 시킬 예정 "" 사이는 무조건 때려 죽어도 포워드 자리. 
+		//인터넷 주소 치면 jsp파일로 인식하여 오류 발생 -> 
+		return "redirect:/phonebook3/list";
+		
+		
+		
 	}
 
 	
 	
 	
-	@RequestMapping(value="/writeForm", method={ RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value="/writeForm", method={RequestMethod.GET, RequestMethod.POST})
 	public String writeForm() {
 		
 		System.out.println("writeForm()");
